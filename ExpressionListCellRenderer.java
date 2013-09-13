c50c8794851d1835fd5c7f97cfd25ea2ec06ab4b
Paul Oser <poser3@emory.edu>
2013-09-13 12:17:16 -0400

    resolved the conflict for real this time

--- ExpressionListCellRenderer.java
+++ ExpressionListCellRenderer.java
@@ -5,15 +5,11 @@ import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 
-//Some random comment
+//Some random comment (I'm harmless -- so I'll stay a while)
 @SuppressWarnings("serial")
 public class ExpressionListCellRenderer extends DefaultListCellRenderer {
 	@Override
-<<<<<<< HEAD
-	public Component getListCellRendererComponentFIXME(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-=======
 	public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
->>>>>>> c27bbe583e86d9887b180eb57537cb4936c92d0b
 		if (value instanceof Expression) {
 			Expression ex = (Expression) value;
 			if (ex.isHidden()) {
