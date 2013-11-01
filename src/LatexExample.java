1586a76902e9d1912909df0893bd3ed75928e6d2
Paul Oser <poser3@emory.edu>
2013-11-01 15:18:47 -0400

    fixed bug where latex was being generated either remotely or not at all

--- src/LatexExample.java
+++ src/LatexExample.java
@@ -21,18 +21,18 @@ public class LatexExample extends GraphicsProgram{
 		latexStrings[1] = "2.\\quad ax^2 + \\fbox{bx + c} = 0";
 		latexStrings[2] = "3.\\quad \\overleftrightarrow{AC} \\textrm{ contains } B";
 		latexStrings[3] = "4.\\quad \\frac{AB}{DE} = \\frac{AC}{DF}";
-		latexStrings[4] = "5.\\quad 2 + {\\color{Blue}\\frac{\\sqrt{x^2+1}}{x-1}} = 7";
+		latexStrings[4] = "5.\\quad 2 + \\frac{\\sqrt{x^2+1}}{x-1} = 7";
 		latexStrings[5] = "6.\\quad m \\angle ABC = m \\angle ABD + m \\angle DBC";
 		
 		//if we just wanted an Image, we call this...
-		Image image = RemoteLatexHandler.latexToImage(latexStrings[0]);
+		Image image = LatexHandler.latexToImage(latexStrings[0]);
 		
 		//if we wanted to play with GImages, instead...
 		GImage[] gImages = new GImage[NUM_OF_LATEX_STRINGS];
 		double maxWidth = 100;
 		double y = V_OFFSET;
 		for (int i = 0; i < latexStrings.length; i++) {
-			gImages[i] = RemoteLatexHandler.latexToGImage(latexStrings[i]);
+			gImages[i] = LatexHandler.latexToGImage(latexStrings[i]);
 			this.add(gImages[i],H_OFFSET,y);
 			y += gImages[i].getHeight() + V_OFFSET;
 			maxWidth = Math.max(maxWidth, gImages[i].getWidth());
@@ -42,3 +42,4 @@ public class LatexExample extends GraphicsProgram{
 	}
 
 }
+
