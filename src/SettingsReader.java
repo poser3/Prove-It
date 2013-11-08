24a616877f3a10eb554517fbbbcb1650245b93ea
Lee Vian <lvian@emory.edu>
2013-11-08 11:04:09 -0500

    default settings are only created once
    The default settings are now stored in a final HashMap, so that they
    don't have to be populated every time the settings file fails to read.
    Also made it possible to manually ask to reload the settings file the
    next time somebody asks for a setting.

--- src/SettingsReader.java
+++ src/SettingsReader.java
@@ -9,11 +9,19 @@ public class SettingsReader {
 	private static boolean fileRead = false;
 	private static HashMap<String, String> settings;
 	
+	private static final HashMap<String, String> defaults = new HashMap<String, String>();
+	static {
+		defaults.put("theorem-path", "../res/theorems/");
+	}
+	
 	public static String getSetting(String key) {
 		if (! fileRead)
 			readFile();
 		
 		return settings.get(key);
+	}
+	public static void resetFileReadStatus() {
+		fileRead = false;
 	}
 	
 	private static void readFile() {
@@ -34,17 +42,17 @@ public class SettingsReader {
 					settings.put(tokens[0], tokens[1]);
 			}
 			
+			fileRead = true;			
 			scanner.close();
 		}
 		catch (FileNotFoundException e) {
 			// Complain
 			System.out.println("Settings file not found!");
+			fileRead = false;
 			
 			// Default values for settings
-			settings.put("theorems-path", "../res/theorems/");
+			settings = defaults;
 		}
-		
-		fileRead = true;
 	}
 
 }
