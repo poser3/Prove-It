71da86d63808faa40a6065d0081ff4c36c0018ae
Lee Vian <lvian@emory.edu>
2013-11-16 15:47:19 -0500

    ExpressionListCellRenderer now uses Statements instead of Expressions. (Breaks the build!)

--- src/ExpressionListCellRenderer.java
+++ src/ExpressionListCellRenderer.java
@@ -6,9 +6,9 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-public class ExpressionListCellRenderer implements ListCellRenderer<Expression> {
+public class ExpressionListCellRenderer implements ListCellRenderer<Statement> {
 	
-	public Component getListCellRendererComponent(JList<? extends Expression> list, Expression value, int index, boolean isSelected, boolean cellHasFocus) {
+	public Component getListCellRendererComponent(JList<? extends Statement> list, Statement value, int index, boolean isSelected, boolean cellHasFocus) {
 		String latex = value.toLatex();
 		if (isSelected)
 			latex = "\\bgcolor{Yellow}{" + latex + "}";
