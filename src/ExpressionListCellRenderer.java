743a89fc6b213652dac8218aeba63c190978f4bc
Lee Vian <lvian@emory.edu>
2013-11-16 15:16:54 -0500

    Now properly renders the entire selected statement in yellow.

--- src/ExpressionListCellRenderer.java
+++ src/ExpressionListCellRenderer.java
@@ -11,7 +11,7 @@ public class ExpressionListCellRenderer implements ListCellRenderer<Expression> 
 	public Component getListCellRendererComponent(JList<? extends Expression> list, Expression value, int index, boolean isSelected, boolean cellHasFocus) {
 		String latex = value.toLatex();
 		if (isSelected)
-			latex = "{\\bgcolor{Yellow}" + latex + "}";
+			latex = "\\bgcolor{Yellow}{" + latex + "}";
 		
 		BufferedImage image = (BufferedImage) LatexHandler.latexToImage(latex);
 		ImageIcon icon = new ImageIcon(image);
